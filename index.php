a95bbe4599e286c7fe235d3d87181e32c85916ef
SavannaWilliams <54858237+SavannaWilliams@users.noreply.github.com>
2020-09-24 17:30:58 -0400

    Add blog posts lists to homepage with PHP.

--- index.php
+++ index.php
@@ -3,9 +3,24 @@
 ?>
 
     <main>
+        <?php
+            function getPostTitlesFromDatabase() {
+                //TODO in Module 4
+                // get this data from a database instead of hardcoding it
+                $postTitles = array("Blog Post 1", "Blog Post 2","Blog Post 3", "Blog Post 4");
+                return $postTitles;
+            }
+        ?>
+
         <h2>Published Posts</h2>
         <ul class="post-list">
-            <li><a href="post.php">Blog Post 1</a></li>
+            <?php
+                $postTitles = getPostTitlesFromDatabase();
+
+                foreach($postTitles as $postTitle) {
+                    echo "<li><a href='post.php?title=" . $postTitle . "'>" . $postTitle . "</a></li>";
+                }
+            ?>
         </ul>
     </main>
 
