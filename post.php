9674beb8122a3a278e5a1e97056286789ad7b174
SavannaWilliams <54858237+SavannaWilliams@users.noreply.github.com>
2020-09-26 16:29:51 -0400

    Add PHP to post page.

--- post.php
+++ post.php
@@ -4,17 +4,31 @@
 
 
     <main>
-        <h2 class="post-title">Blog Post 1</h2>
+
+    <?php
+        function getPostDetailsFromDatabase() {
+            // TODO in Module 4
+            // get this data from a database instead of hardcoding it
+            $postDetails = array('title' => 'Blog Post 1',
+                                'content' => 'My first blog post.',
+                                'date' => '01/01/2020',
+                                'author' => 'Savanna Williams');
+                                return $postDetails;
+        }
+
+        // Post details contains all the data to generate the blog from
+        $postDetails = getPostDetailsFromDatabase();
+    ?>
+
+        <h2 class="post-title"><?php echo $postDetails["title"]; ?></h2>
         <div class="post-publish-date">
-            <p>09/24/20</p>
+            <p><?php echo $postDetails["date"]; ?></p>
         </div>
         <div class="post-author">
-            <p>Savanna Williams</p>
+            <p><?php echo $postDetails["author"]; ?></p>
         </div>
         <div class="post-content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id mauris tincidunt, facilisis sapien at, consequat dolor. Phasellus placerat placerat fringilla. Donec id ultricies nunc. Praesent id velit ut quam laoreet aliquet. Integer quis lectus ullamcorper libero mattis scelerisque. In rutrum ultricies tortor sed scelerisque. Etiam vehicula mauris et mauris mattis efficitur. Fusce nec consectetur nulla.</p>
-            <p>Fusce vel massa ac elit rutrum condimentum. Nam pretium, tellus non dignissim luctus, sapien purus congue nunc, in vehicula neque nunc eu orci. Vivamus feugiat tellus sit amet laoreet malesuada. Phasellus lacinia odio nisl, sit amet rutrum lacus rhoncus quis. Integer ut est consequat, commodo nisi ut, vehicula diam. Nunc laoreet massa sapien, a hendrerit felis cursus a. Donec id hendrerit ante, ut ornare dui. Quisque lacinia nulla ac posuere facilisis. Duis in arcu sed quam tincidunt lacinia at tincidunt metus. Aenean fermentum nisi facilisis, pharetra dolor at, laoreet tortor. Pellentesque mattis non dui vitae placerat. Nam a nisi tortor. Donec placerat massa in justo egestas, aliquet ornare orci volutpat. Sed sit amet orci id libero mollis molestie. Nunc aliquam, mauris vel rutrum viverra, elit magna imperdiet augue, at faucibus felis lorem at mi. Donec commodo cursus orci at ullamcorper.</p>
-            <p>Vestibulum bibendum diam enim, convallis commodo dui mollis ut. Donec quis lacinia urna, at pretium nibh. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce ultricies dolor quis nisi ultrices, vitae volutpat lectus sodales. Pellentesque id ex dignissim, tempus risus ut, ullamcorper ligula. Integer molestie nisi nec porta pretium. Vivamus semper ut elit et maximus. Cras bibendum diam a suscipit ultricies. Nunc ultricies, nisi pretium placerat tempus, nisi lectus sodales tellus, iaculis volutpat diam velit eget sem. In pulvinar, justo in aliquam mattis, nulla tortor porta turpis, rhoncus tincidunt felis metus a turpis. Phasellus ornare nibh ac dapibus elementum. Vestibulum et neque id nisi laoreet cursus nec eu lacus. Suspendisse sapien est, suscipit a nisl quis, convallis hendrerit purus. In tincidunt congue gravida. Ut sit amet interdum magna.</p>
+            <p><?php echo $postDetails["content"]; ?></p>
         </div>
     </main>
 
